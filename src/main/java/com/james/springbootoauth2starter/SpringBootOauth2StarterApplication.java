b73ebdd7f16a3cb78e41af69100b5aa12e58892d
jamesxu182 <jamesxu182@gamil.comgit config --global user.name jamesxu182git config --global user.email jamesxu182@gamil.comgit config --global user.email jamesxu182@gamil.com>
2017-10-17 15:26:01 +0100

    uploaded an empty project

---
+++ src/main/java/com/james/springbootoauth2starter/SpringBootOauth2StarterApplication.java
@@ -0,0 +1,12 @@
+package com.james.springbootoauth2starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2StarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2StarterApplication.class, args);
+	}
+}
